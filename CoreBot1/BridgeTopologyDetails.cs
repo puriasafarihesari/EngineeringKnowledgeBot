a6f5680eedcf7d968d3e389f6590dce35b6148a6
Puria Safari Hesari <pihise@ramboll.dk>
2020-02-23 00:07:33 +0100

    Initial commit to the state successfully deploying

---
+++ CoreBot1/BridgeTopologyDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBot1
+{
+    public class BridgeTopologyDetails
+    {
+        public string Country { get; set; }
+        //public string Span { get; set; }
+    }
+}
